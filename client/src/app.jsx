8e81dd19f0e5f0768b6872dd6cb32129de86f778
Donald Vida <donald.vida@gmail.com>
2021-10-12 18:04:51 -0500

    setup wire-framing for the app

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import './styles.css';
 
+import {
+  Box,
+  Container,
+  Grid,
+  Paper,
+  Typography,
+} from '@mui/material';
+
 const App = () => (
-  <div className="App">
-    <h1> Hello, World! </h1>
-  </div>
+  < >
+    <Box maxWidth className="titlebar">
+      <Typography variant="h5">
+        EatPlanner
+      </Typography>
+    </Box>
+    <Container maxWidth="lg" className="app">
+      <Grid container spacing={3}>
+        <Grid item sm={12} lg={4}>
+          <Paper elevation={2} className="menuweek">
+            Meal Plan
+          </Paper>
+        </Grid>
+        <Grid item sm={12} lg={8}>
+          <Paper elevation={2} className="menuweek">
+            Recipe Search
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  </>
 );
 
 export default App;
